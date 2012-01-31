1087cf8eb4bfe4652f7c8d1a8ffa2065af5609ff
booo <borgers@mi.fu-berlin.de>
2012-01-31 23:20:10 +0100

    add travis as ci system

---
+++ travis/before_install.sh
@@ -0,0 +1,35 @@
+sudo aptitude install \
+    scons\
+    git-core\
+    python-support\
+    debhelper\
+    python-all-dev\
+    libboost-thread-dev\
+    libboost-filesystem-dev\
+    libboost-regex-dev\
+    libboost-python-dev\
+    libboost-system-dev\
+    libboost-iostreams-dev\
+    libboost-program-options-dev\
+    libpng12-dev\
+    libjpeg62-dev\
+    libtiff4-dev\
+    zlib1g-dev\
+    libfreetype6-dev\
+    libpq-dev\
+    libproj-dev\
+    libltdl7-dev\
+    libgdal1-dev\
+    libxml2-dev\
+    libicu-dev\
+    libcairo2-dev\
+    libcairomm-1.0-dev\
+    python-cairo-dev\
+    libsqlite3-dev\
+    libcurl4-gnutls-dev\
+    libsigc++-2.0-dev\
+    && git clone https://github.com/mapnik/mapnik \
+    && cd mapnik \
+    && ./configure \
+    && ./make -j4 \
+    && cd ..
